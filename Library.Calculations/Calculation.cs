9711e83e8a769c16d11febd7e1a41ed5fb967b10
nirajdahal09@gmail.com <nirajdahal09@gmail.com>
2020-08-12 21:31:24 +0545

    Add project files.

---
+++ Library.Calculations/Calculation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Calculations
+{
+    public class Calculation
+    {
+        public double Sum(double a , double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multipication(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a/b;
+        }
+    }
+}
---
+++ Library.Tests/CalculationsTest.cs
@@ -0,0 +1,33 @@
+﻿using Library.Calculations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Library.Tests
+{
+
+    //Arrange, Act, Assert
+    //expected
+    
+    public class CalculationsTest
+    {
+        
+        [Theory]
+        [InlineData(2,3,5)]
+        [InlineData(541, 3, 544)]
+        [InlineData(5,5, 10)]
+        public void ShouldReturnAddedValueAsExpected(double x, double y, double expected)
+        {
+            //Arrange
+            Calculation calc = new Calculation();
+
+            //Act
+            double actual= calc.Sum(x, y);
+
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
